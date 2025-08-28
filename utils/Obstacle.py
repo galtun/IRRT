ea3acb8c3736f44d4ee4a756b1695312caceb8be
Gökhan <altungokhan@live.com>
2025-08-28 15:36:21 +0300

    Update Obstacle.py

--- utils/Obstacle.py
+++ utils/Obstacle.py
@@ -2,6 +2,6 @@ class Obstacle:
     def __init__(self, pointStart, pointEnd, obsType = 1, moveType = 1):
         self.pointStart = pointStart
         self.pointEnd = pointEnd
-        self.obsType = obsType # 1 dikdörtgen engel - 0 dairesel engel
-        self.moveType = moveType # 0 sabit engel - 1 hareketli engel
+        self.obsType = obsType # 1 rectangular obstacle - 0 circular obstacle
+        self.moveType = moveType # 0 fixed obstacle - 1 moving obstacle
         self.obsticleList = []
